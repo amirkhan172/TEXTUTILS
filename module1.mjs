178eb9d90652d47800793683e8c65e169fc1eaed
Amir Khan <amir77husan@gmail.com>
2025-05-14 12:20:13 +0500

    Updates

--- module1.mjs
+++
@@ -1,8 +0,0 @@
-import dz, {a, b, c} from './module2.mjs'
-console.log(dz)
-console.log(c)
-console.log(a)
-console.log(b)
-
-// import ui from './module2.mjs'
-// console.log(ui) // mai "ui" ki jaga kuch b likh skta hu.
--- module2.mjs
+++
@@ -1,9 +0,0 @@
-const a = "harry";
-const b = "Rohan";
-const c = "Akash";
-const d = "Priyanka";
-
-export default c;
-export {a};
-export {b};
-export {c};
